57a139487a1f8b9fa745e27718f6bfbcf8d3cf7c
unknown <ngockhanhbg97@gmail.com>
2023-04-26 15:44:31 +0700

    update

--- ultils/ultils.js
+++ ultils/ultils.js
@@ -27,22 +27,34 @@ exports.checkError = (validationRules, req, body) => {
 // Function to get data from cache by cacheKey
 exports.getCacheData = async (cacheKey) => {
   return new Promise((resolve, reject) => {
-    client.get(cacheKey, (err, data) => {
-      if (err) {
-        reject(err);
+    client.on("ready", async () => {
+      try {
+        client.get(cacheKey, (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(data);
+        });
+      } catch (err) {
+        console.error(err);
       }
-      resolve(data);
     });
   });
 };
 // Function to set data to cache with cacheKey and time-to-live (TTL)
 exports.setCachedData = async (cacheKey, data, ttl) => {
   return new Promise((resolve, reject) => {
-    client.hmset(cacheKey, ttl, JSON.stringify(data), (err) => {
-      if (err) {
-        reject(err);
+    client.on("ready", async () => {
+      try {
+        client.hmset(cacheKey, ttl, JSON.stringify(data), (err) => {
+          if (err) {
+            reject(err);
+          }
+          resolve();
+        });
+      } catch (err) {
+        console.error(err);
       }
-      resolve();
     });
   });
 };
